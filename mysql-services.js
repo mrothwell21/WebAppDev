52d46f0a4a85d1c9cbd30cbcee80e2246bc79e4e
rockd <rockdaniel87@yahoo.com>
2024-10-09 16:48:02 -0400

    copied professors web-server and mysql services as template

---
+++ mysql-services.js
@@ -0,0 +1,36 @@
+MediaSourceHandle.exports = db = {
+    mydb : {
+        host: "localhost",
+        user: "root",
+        password: "**********",
+        database: "mydb"
+    },
+
+    selectAll: async function (conn, tableName) {
+        const results = await conn.promise().query(`SELECT * FROM ${tableName}`);
+
+        return results[0];
+    },
+
+    //can add role later if needed
+    getOne : async function(conn, tableName, name, password) {
+        const sql = `SELECT * FROM ${tableName} WHERE name = '${name}' AND
+        password = '${password}'`;
+        const results = await conn.promise().query(sql);
+
+        return results[0];
+    },
+
+    addOne : async function(conn, tableName, user) {
+
+        const newValues = `"${user.name}", "${user.address}",
+        "${user.password}"`;
+
+        const sql = `INSERT INTO ${tableName} (name,address,password)
+        Values (${newValues})`;
+
+        const results = await conn.promise().query(sql);
+
+        return results;
+    }
+};
--- web-server.js
+++ web-server.js
@@ -1,7 +1,16 @@
 const express  = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+const router = express.Router();
+
+router.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static("public"));
+
+//router.use("/api", require("./api/users"));
+
+app.use(router);
 
 app.listen(3000, function() {
     console.log("Listening on port 3000...");
