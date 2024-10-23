4be71971b7f440f6cca246a16de2a1d7907d0fc5
mrothwell21 <mroth5@brockport.edu>
2024-10-23 16:07:49 -0400

    password button and local storage clear

--- public/javascript/user.js
+++ public/javascript/user.js
@@ -1,10 +1,16 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
     let logoutButton = document.getElementById("logout");
+    let passwordButton = document.getElementById("password");
 
 
     logoutButton.addEventListener("click", function (e) {
+        localStorage.clear();
         window.location.assign("./index.html");
 
     });
+
+    passwordButton.addEventListener("click", function (e){
+        window.location.assign("./ChangePassword.html");
+    })
 });
