2a3021ba5986e87f52537168ec9f22fd4399fef4
rockd <rockdaniel87@yahoo.com>
2024-11-04 18:25:24 -0500

    connected index files

---
+++ client/src/index.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+import { AuthProvider } from './contexts/AuthContext.jsx';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <StrictMode>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </StrictMode>
+)
+
