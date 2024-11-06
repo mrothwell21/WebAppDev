9435b9ee8a2629dfdbb2866655b110140ab20a71
nlime1 <nikki.limer@gmail.com>
2024-11-06 17:34:39 -0500

    added new login files

---
+++ client/src/hooks/useLogin.jsx
@@ -0,0 +1,36 @@
+import { React, useState } from "react";
+import { useAuth } from "../contexts/AuthContext.jsx";
+import { message } from "antd";
+
+
+const useLogin = () => {
+    const { login } = useAuth() || { login: null };
+    const [error, setError] = useState(null);
+    const loginUser = async (values) => {
+        try {
+            setError(null);
+            const res = await fetch('http://localhost:3000/api/auth/login',
+                {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(values)
+                }
+            );
+            const data = await res.json();
+            if (res.status === 200) {
+                message.success('User logged-in successfully');
+                await login(data.token, data.user);
+            }
+            else if (res.status === 404) {
+                message.error('No such user');
+            }
+            else {
+                message.error('Login failed');
+            }
+        }
+        catch (err) { message.error(err); }
+    };
+    return ({ error, loginUser });
+}
+export default useLogin;
+client / hooks / useLogin.js
