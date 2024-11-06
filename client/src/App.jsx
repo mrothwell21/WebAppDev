f997bc5179c200894d4a603fee33b10baa1547ce
nlime1 <nikki.limer@gmail.com>
2024-11-06 17:53:18 -0500

    used her login files to help set up login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,12 +18,29 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  const { isAuthenticated, userData } = useAuth() || { isAuthenticated: false, userData: null };
+  const userRole = (isAuthenticated) ? userData.role : 'user';
+  const dashboard = toDashboard(isAuthenticated, userRole);
+
   return (
-    <>
-    <RouterProvider router={router} />
-    
-    </>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to={dashboard.to} />} />
+        <Route path='/login' element={<Navigate to={dashboard.to} />} />
+        <Route path='/dashboard-admin' element={isAuthenticated && userRole === 'admin' ? <DashboardAdmin /> : <Navigate to={dashboard.to} />} />
+        <Route path='/dashboard-teacher' element={isAuthenticated && userRole === 'teacher' ? <DashboardTeacher /> : <Navigate to={dashboard.to} />} />
+        <Route path='/dashboard-student' element={isAuthenticated && userRole === 'student' ? <DashboardStudent /> : <Navigate to={dashboard.to} />} />
+      </Routes>
+    </BrowserRouter>
+    );
+  };
+
+  function toDashboard(isAuthenticated, role) {
+    if (isAuthenticated && role === 'student') { return { to: '/dashboard-student', dash: <DashboardStudent /> }; }
+    if (isAuthenticated && role === 'teacher') { return { to: '/dashboard-teacher', dash: <DashboardTeacher /> }; }
+    if (isAuthenticated && role === 'admin') { return { to: '/dashboard-admin', dash: <DashboardAdmin /> }; }
+
+    return { to: '/login', dash: <Login /> };
 }
 
 export default App;
---
+++ client/src/contexts/AuthContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import Login from "../Auth/Login";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [token, setToken] = useState(null);
+    const [userData, setUserData] = useState(null);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const locallyStoredData = JSON.parse(localStorage.getItem('userData'));
+
+    useEffect(() => {
+        if (locallyStoredData) {
+            const { userToken, user } = locallyStoredData;
+            setToken(userToken);
+            setUserData(user);
+            setIsAuthenticated(true);
+        }
+    }, []);
+
+    const login = (newToken, newData) => {
+        localStorage.setItem('userData', JSON.stringify({ userToken: newToken, user: newData }));
+        setToken(newToken);
+        setUserData(newData);
+        setIsAuthenticated(true);
+    };
+
+    const values = { token, isAuthenticated, login, userData, setUserData };
+    
+    return (
+        <AuthContext.Provider value={{ ...values }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+export const useAuth = () => { return useContext(AuthContext) };
+
