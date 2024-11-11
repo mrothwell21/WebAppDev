683b2be4abe55ac67df3fd3842cab5d4812709fd
mrothwell21 <mroth5@brockport.edu>
2024-11-11 14:04:47 -0500

    new hook

---
+++ client/src/hooks/getUser.jsx
@@ -0,0 +1,37 @@
+import { React, useState } from "react";
+import { useAuth } from "../contexts/AuthContext.jsx";
+import { message } from "antd";
+
+
+const getUser = () => {
+    const { login } = useAuth() || { login: null };
+    const [error, setError] = useState(null);
+
+    const getUser = async (values) => {
+        try {
+            setError(null);
+
+            const res = await fetch('http://localhost:5050/api/users/status',
+                {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(values)
+                }
+            );
+
+            const data = await res.json();
+            if (res.status === 200) {
+                await user(data);
+            }
+            else if (res.status === 404) {
+                message.error('No such user');
+            }
+            else {
+                message.error('Login failed');
+            }
+        }
+        catch (err) { message.error(err); }
+    };
+    return ({ error, getUser });
+}
+export default useLogin;
