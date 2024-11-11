46636dd940a6e18321dc80f559d5f440002f59bb
nlime1 <nikki.limer@gmail.com>
2024-11-11 16:14:34 -0500

    inital css imported for teacher and admin

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import '../../public/css/adminRole.css'
 
 function AdminPage() {
     return (
--- client/src/pages/TeacherPage.jsx
+++ client/src/pages/TeacherPage.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import '../../public/css/TeacherLanding.css'
 
 function TeacherPage() {
     return (
