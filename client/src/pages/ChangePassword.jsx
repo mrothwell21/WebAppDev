fb5bce04b24fe3d6ef72a2c29241a3cc1ea86428
mrothwell21 <mroth5@brockport.edu>
2024-11-12 12:55:38 -0500

    change password logic

--- client/src/pages/ChangePassword.jsx
+++ client/src/pages/ChangePassword.jsx
@@ -2,13 +2,59 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import React from 'react';
 import { useAuth } from "../contexts/AuthContext";
+import { useState } from "react";
 
 function ChangePassword() {
     const { userData, isAuthenticated, logout } = useAuth();
     const navigate = useNavigate();
     const token = localStorage.getItem("userData");
+    const [inputs, setInputs] = useState({});
 
-    console.log(userData);
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+
+        const response = await fetch("http://localhost:5050/api/password/change", {
+            method: "POST",
+            body: new URLSearchParams({ username: inputs.username, currentPassword: inputs.currentPassword, newPassword: inputs.newPassword, confirimPassword: inputs.confirmPassword })
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+            localStorage.setItem("userData", data.token);
+
+            switch (userData.role) {
+                case 1:
+                    navigate("/dashboard-admin");
+                    break;
+                case 2:
+                    navigate("/dashboard-teacher");
+                    break;
+                case 3:
+                    navigate("/dashboard-student");
+                    break;
+                default:
+                    logout();
+                    break;
+            }
+
+        }
+
+    }
+
+
+    const handleChange = (event) => {
+        const username = event.target.username;
+        const nPass = event.target.newPassword;
+        const curPass = event.target.currentPassword;
+        const conPass = event.target.confirmPassword;
+
+        setInputs(values => ({ ...values, [username]: username, [newPassword]: nPass, [currentPassword]: curPass, [confirmPassword]: conPass }))
+    }
+
+
 
     function handleBack() {
         switch (userData.role) {
@@ -33,7 +79,7 @@ function ChangePassword() {
         <div className="window">
             <div className="window-header">Change Password</div>
             <div className="password-container">
-                <form action="#" method="POST">
+                <form onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label for="username">Username:</label>
                         <input type="text" id="username" name="username" value={userData.username} required disabled />
@@ -41,17 +87,17 @@ function ChangePassword() {
 
                     <div className="form-group">
                         <label for="current-password">Current Password:</label>
-                        <input type="password" id="current-password" name="current-password" required />
+                        <input type="password" id="currentPassword" name="currentPassword" onChange={handleChange} required />
                     </div>
 
                     <div className="form-group">
                         <label for="new-password">New Password:</label>
-                        <input type="password" id="new-password" name="new-password" required />
+                        <input type="password" id="newPassword" name="newPassword" onChange={handleChange} required />
                     </div>
 
                     <div className="form-group">
                         <label for="confirm-password">Retype New Password:</label>
-                        <input type="password" id="confirm-password" name="confirm-password" required />
+                        <input type="password" id="confirmPassword" name="confirmPassword" onChange={handleChange} required />
                     </div>
 
                     <button type="submit" id="submit">Submit</button>
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const authController = require('../controllers/users');
+const passController = require('../controllers/password');
 const router = express.Router();
 
 //router.post('/login', authController.login);
 router.use("/api/users", authController);
+router.use("/api/password", passController);
 
 module.exports = router;
