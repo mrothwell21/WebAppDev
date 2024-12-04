5f6fc77db3885053d4e94e42ce2b52ed7c6b2885
mrothwell21 <mroth5@brockport.edu>
2024-12-04 16:32:40 -0500

    drop and register course hooks and router calls

---
+++ client/src/hooks/dropCourse.js
@@ -0,0 +1,33 @@
+import { React } from "react";
+import { useAuth } from "../contexts/AuthContext";
+
+const dropCourses = () => {
+    const { userData } = useAuth();
+
+    const dropCourse = async (courseId, majorPrefix) => {
+        try {
+            // console.log(selectedMajor);
+            const response = await fetch(`http://localhost:5050/api/student-courses/drop}`, {
+                method: 'POST',
+                body: new URLSearchParams({ username: userData.username, courseId: courseId, prefix:majorPrefix})
+            });
+
+            // console.log(response);
+
+            if (response.ok) {
+                const data = await response.json();
+                return data;
+            } else {
+                console.error('Failed to fetch courses');
+                return [];
+            }
+        } catch (error) {
+            console.error('Error fetching courses:', error);
+            return [];
+        }
+    };
+
+    return ({dropCourse})
+};
+
+export default dropCourses;
--- server/controllers/student-courses.js
+++ server/controllers/student-courses.js
@@ -78,6 +78,32 @@ router.post("/register", async function (req, res) {
     }
 });
 
+router.post("/drop", async function (req, res) {
+    // console.log("in fetch")
+    if (!req.headers["x-auth"]) {
+        return res.status(401).json({ error: "Missing X-Auth header" });
+    }
+
+    const token = req.headers["x-auth"];
+    // console.log("x-auth");
+
+    try {
+        // const decoded = jwt.decode(token, secret);
+        const conn = mysql.createConnection(db.mydb);
+        // console.log("connection")
+        const response = await db.dropCourse(conn, req.body.username, req.body.courseId, req.body.prefix);
+        // console.log(results);
+        if (!results || results.length === 0) {
+            return res.status(404).json({ error: "No Courses found for this Major" });
+        }
+        res.status(200).json(results);
+    }
+    catch (ex) {
+        console.error("Error getting courses:", ex);
+        res.status(401).json({ error: "Invalid JWT or database error" });
+    }
+});
+
 router.get("/enrolled/:username/:majorId", async function (req, res) {
     //console.log("in fetch")
    if (!req.headers["x-auth"]) {
